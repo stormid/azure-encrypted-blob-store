7a18756f2aaa9897973eb674cbad94452cad5cfc
philo <phil@stormid.com>
2015-10-22 18:23:52 +0100

    Updates nuspec

--- src/AzureEncryptedBlobStore/Properties/AssemblyInfo.cs
+++ src/AzureEncryptedBlobStore/Properties/AssemblyInfo.cs
@@ -5,12 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AzureEncryptedBlobStore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Azure Encrypted Blob Store")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Storm ID")]
 [assembly: AssemblyProduct("AzureEncryptedBlobStore")]
 [assembly: AssemblyCopyright("Copyright © Storm ID 2015")]
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
